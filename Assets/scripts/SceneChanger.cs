8fb16440646919b5b91b9f8c13aeb05ed8a18901
Chuan Tan <ct538@cam.ac.uk>
2020-04-27 19:36:39 +0800

    reorgianised

---
+++ Assets/scripts/SceneChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class SceneChanger : MonoBehaviour {
+    public void Left()
+    {
+        SceneManager.LoadScene("SpaceBase");
+    }
+    public void Center()
+    {
+        SceneManager.LoadScene("OutdoorScene");
+    }
+    public void Right()
+    {
+        SceneManager.LoadScene("CarInterior");
+    }
+    public void SecretBehind()
+    {
+        SceneManager.LoadScene("VRScene");
+    }
+
+}
